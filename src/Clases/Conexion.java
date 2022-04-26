87f1c5bd57f88dedc55897761ea4db27ef91bb25
felix <felix@DESKTOP-25UUM7Q.mshome.net>
2022-04-26 17:50:56 -0400

    login y registrar usuarios incompletos

--- src/Clases/Conexion.java
+++ src/Clases/Conexion.java
@@ -8,6 +8,27 @@ package Clases;
  *
  * @author felix
  */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 public class Conexion {
+    public static String BD = "jdbc:postgresql://localhost:5432/prueba";
+    public static String USER = "postgres";
+    public static String PASSWORD = "admin";
+    
+    public static Connection GetConexion(){
+        
+        try {
+            Connection conexion = DriverManager.getConnection(BD,USER,PASSWORD);
+            //JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+            return conexion;
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "error:"+e);
+            return null;
+        }
+        
+        
+    }
     
 }
---
+++ src/Clases/Funciones.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Felix Artiles
+ */
+public class Funciones {
+
+    public static final Conexion cc = new Conexion();
+    public static final Connection cn = Conexion.GetConexion();
+    public static String sql = null;
+    public static String nombre_formulario = "";
+    public boolean empleado_usuario = false;
+    public static String fecha_i, fecha_f;
+    public static String[] dato = new String[5];
+    private static PreparedStatement ps;
+    
+    public static void guardar_datos(String sql){
+        
+            try {
+                
+                Conexion.GetConexion();
+                Statement st = Conexion.GetConexion().createStatement();
+              
+               st.executeUpdate(sql);
+             
+               
+                ps = cn.prepareStatement(sql);
+                ResultSet rs =ps.executeQuery();
+                   
+              
+                JOptionPane.showMessageDialog(null, "Se ha guardado correctamente.", "Registro", JOptionPane.INFORMATION_MESSAGE);
+                
+               
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e, "error", JOptionPane.ERROR_MESSAGE);
+            
+        }    
+    }
+    
+    public static ResultSet consulta(String consulta) {
+        Statement st;
+        ResultSet datos = null;
+        try {
+            st = cn.createStatement();
+            datos = st.executeQuery(consulta);
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return datos;
+
+    }
+
+    public static void validar_correo(JTextField correo) {
+        Pattern expresion = Pattern
+                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher mather = expresion.matcher(correo.getText());
+        if (!mather.find()) {
+            JOptionPane.showMessageDialog(null, "El correo " + correo.getText() + " esta mal escrito.", "", JOptionPane.ERROR_MESSAGE);
+            correo.requestFocus();
+        }
+
+    }
+
+    public static void llenar_combo(JComboBox combo, String tabla, String columna) {
+        sql = "SELECT * from " + tabla;
+        combo.removeAllItems();
+        ResultSet rs = Funciones.consulta(sql);
+        try {
+
+            while (rs.next()) {
+                combo.addItem(rs.getString(columna));
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e, "", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void solo_numeros(JTextField campo) {
+        campo.addKeyListener(new KeyAdapter() {
+            public void KeyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!Character.isDigit(c)) {
+                    e.consume();
+                }
+            }
+        });
+    }
+
+    public static void limitar_caracteres(JTextField campo, int total) {
+        campo.addKeyListener(new KeyAdapter() {
+            public void KeyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                int cant = campo.getText().length();
+                if (cant >= total) {
+                    e.consume();
+                }
+            }
+
+        });
+    }
+
+    public static String fecha() {
+        Date fecha = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
+
+        return sdf.format(fecha);
+    }
+
+    public static boolean buscar_usuario(String usuario) {
+        boolean encontrado = false;
+        sql = "select nickname from usuario where nickname ='" + usuario + "'";
+        ResultSet dato = consulta(sql);
+
+        try {
+            if (dato.next()) {
+                encontrado = true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return encontrado;
+    }
+
+    public static int traeindice(String tabla, String descripcion) {
+
+        sql = "SELECT * from " + tabla + " WHERE descripcion ='" + descripcion + "'";
+     
+        ResultSet rs = Funciones.consulta(sql);
+        try {
+            rs.next();
+
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e, "funciones", JOptionPane.ERROR_MESSAGE);
+        }
+        return 0;
+    }
+}
